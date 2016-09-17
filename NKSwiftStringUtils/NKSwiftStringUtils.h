def63c27d8d47bbc0c927903bd5578af42d68f6e
Naresh <naresh@naresh-macbook-air.local>
2016-09-17 23:10:15 +0530

    Initial Commit

---
+++ NKSwiftStringUtils/NKSwiftStringUtils.h
@@ -0,0 +1,19 @@
+//
+//  NKSwiftStringUtils.h
+//  NKSwiftStringUtils
+//
+//  Created by Naresh on 17/09/16.
+//  Copyright © 2016 Naresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NKSwiftStringUtils.
+FOUNDATION_EXPORT double NKSwiftStringUtilsVersionNumber;
+
+//! Project version string for NKSwiftStringUtils.
+FOUNDATION_EXPORT const unsigned char NKSwiftStringUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKSwiftStringUtils/PublicHeader.h>
+
+
